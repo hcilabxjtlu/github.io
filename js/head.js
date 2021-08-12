a380d3cdf42a472dde942dfdaa97948187ac6c60
hcilabxjtlu <88577339+hcilabxjtlu@users.noreply.github.com>
2021-08-12 11:48:20 +0800

    Add files via upload

---
+++ js/head.js
@@ -0,0 +1,10 @@
+document.writeln('<div class="nav-scroller py-1 mb-2">');  
+document.writeln('<nav class="nav d-flex justify-content-between">');  
+document.writeln('      <a class="p-2 link-secondary" href="#">Index</a>');  
+document.writeln('      <a class="p-2 link-secondary" href="https://hcilabxjtlu.github.io/homepage/blogs.html">Blogs</a>');  
+document.writeln('      <a class="p-2 link-secondary" href="#">Publications</a>');  
+document.writeln('      <a class="p-2 link-secondary" href="#">Members</a>');  
+document.writeln('      <a class="p-2 link-secondary" href="#">Contact Us</a>');  
+document.writeln('    </nav>');  
+document.writeln('  </div>');  
+document.writeln('</div>');  
