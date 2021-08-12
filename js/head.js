563f1c20f6869cd1b3e5c84164abd959e85673e4
hcilabxjtlu <88577339+hcilabxjtlu@users.noreply.github.com>
2021-08-12 12:05:45 +0800

    Update head.js

--- js/head.js
+++ js/head.js
@@ -1,5 +1,5 @@
-document.writeln('<div class="nav-scroller py-1 mb-2">');  
-document.writeln('<nav class="nav d-flex justify-content-between">');  
+document.writeln('  <div class="nav-scroller py-1 mb-2">');  
+document.writeln('    <nav class="nav d-flex justify-content-between">');  
 document.writeln('      <a class="p-2 link-secondary" href="https://hcilabxjtlu.github.io/homepage/index.html">Index</a>');  
 document.writeln('      <a class="p-2 link-secondary" href="https://hcilabxjtlu.github.io/homepage/blogs.html">Blogs</a>');  
 document.writeln('      <a class="p-2 link-secondary" href="#">Publications</a>');  
@@ -7,4 +7,3 @@ document.writeln('      <a class="p-2 link-secondary" href="#">Members</a>');
 document.writeln('      <a class="p-2 link-secondary" href="#">Contact Us</a>');  
 document.writeln('    </nav>');  
 document.writeln('  </div>');  
-document.writeln('</div>');  
