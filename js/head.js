9e27f98d638ccb73e3b959c3178c08748268b984
hcilabxjtlu <88577339+hcilabxjtlu@users.noreply.github.com>
2021-10-02 00:08:16 +0800

    Update head.js

--- js/head.js
+++ js/head.js
@@ -8,7 +8,7 @@ document.writeln('        <a class="blog-header-logo text-dark" href="#">xrVis</
 document.writeln('      </div>');  
 document.writeln('      <div class="col-4 d-flex justify-content-end align-items-center">');  
 document.writeln('        <a class="link-secondary" href="#" aria-label="Search">');  
-document.writeln('          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img" viewBox="0 0 24 24"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"/><path d="M21 21l-5.2-5.2"/></svg>');  
+//document.writeln('          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img" viewBox="0 0 24 24"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"/><path d="M21 21l-5.2-5.2"/></svg>');  
 document.writeln('        </a>');  
 document.writeln('        <a class="link-secondary" href="#"></a>');  
 document.writeln('      </div>');  
