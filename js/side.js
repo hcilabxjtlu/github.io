8f1213a95ccad70ace6b24bbff17739a63290a31
hcilabxjtlu <88577339+hcilabxjtlu@users.noreply.github.com>
2021-10-04 11:35:31 +0800

    Update side.js

--- js/side.js
+++ js/side.js
@@ -3,7 +3,7 @@ document.writeln('      <div class="position-sticky" style="top: 2rem;">');
 document.writeln('        <div class="p-4 mb-3 bg-light rounded">');  
 document.writeln('          <h4 class="fst-italic">Guide</h4>');  
 document.writeln('          <ol class="list-unstyled mb-0">'); 
-document.writeln('            <li><a href="https://hcilabxjtlu.github.io/homepage/meetingschedule.html">Group Meeting Schedule</a></li>'); 
+document.writeln('            <li><a href="meetingschedule.html">Group Meeting Schedule</a></li>'); 
 document.writeln('          </ol>'); 
 document.writeln('        </div>');  
 document.writeln('        <div class="p-4">');  
